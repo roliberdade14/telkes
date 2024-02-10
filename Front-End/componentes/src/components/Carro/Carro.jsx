2a84970b52193dd49706539bd84a0db4a45130a8
julianaMoraes <juliana.hmoraes@gmail.com>
2024-02-10 09:12:35 -0300

    aula de componentes

---
+++ Front-End/componentes/src/components/Carro/Carro.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Carro = () => {
+
+    return (
+        <>
+            <h2>Fiat Toro</h2>
+            <ol>
+                <li>Cor: Vermelho</li>
+                <li>Ano: 2023</li>
+                <li>Flex: Sim</li>
+            </ol>
+        </>
+    )
+
+}
+
+export default Carro
